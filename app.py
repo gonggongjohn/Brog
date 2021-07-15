a054d5e693361e9b2ae5f704c7bf235f29099f1d
shenxiaoqi-1028 <1172685551@qq.com>
2021-07-15 13:24:46 +0800

    写了一些静态前端

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from be.model.route import bp
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
 
+
 if __name__ == '__main__':
     app = Flask(__name__)
     app.register_blueprint(bp)
