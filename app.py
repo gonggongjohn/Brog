8e9c86189e8399750f1ee26536c88216e6bbc2cc
shenxiaoqi-1028 <1172685551@qq.com>
2021-07-19 19:25:37 +0800

    用bootstrap框架，写了注册登录和上传

--- app.py
+++ app.py
@@ -1,40 +1,177 @@
-#!/usr/bin/env python3
 
 import os
-import sys
-from flask import Flask, request, Response, jsonify, render_template
-from be.model.database import init_db
-from be.model.route import bp
+import uuid
+
+from flask import Flask, render_template, flash, redirect, url_for, request, send_from_directory, session
+from flask_ckeditor import CKEditor, upload_success, upload_fail
 from flask_dropzone import Dropzone
+from flask_wtf.csrf import validate_csrf
+from wtforms import ValidationError
 
-file_dir = os.path.dirname(__file__)
-sys.path.append(file_dir)
+from forms import LoginForm, FortyTwoForm, NewPostForm, UploadForm, MultiUploadForm, SigninForm, \
+    RegisterForm, SigninForm2, RegisterForm2, RichTextForm
 
-@bp.route('/upload', methods=['POST', 'GET'])
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY', 'secret string')
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+# Custom config
+app.config['UPLOAD_PATH'] = os.path.join(app.root_path, 'uploads')
+
+if not os.path.exists(app.config['UPLOAD_PATH']):
+    os.makedirs(app.config['UPLOAD_PATH'])
+
+app.config['ALLOWED_EXTENSIONS'] = ['pdf', 'txt']
+
+# Flask config
+# set request body's max length
+# app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024  # 3Mb
+
+# Flask-CKEditor config
+app.config['CKEDITOR_SERVE_LOCAL'] = True
+app.config['CKEDITOR_FILE_UPLOADER'] = 'upload_for_ckeditor'
+
+# Flask-Dropzone config
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.pdf, .txt'
+app.config['DROPZONE_MAX_FILE_SIZE'] = 5
+app.config['DROPZONE_MAX_FILES'] = 30
+
+ckeditor = CKEditor(app)
+dropzone = Dropzone(app)
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])#注册页面
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        # username = form.username.data
+        # flash('Welcome home, %s!' % username)
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])#登录页面
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        flash('Welcome home, %s!' % username)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/index', methods=['GET', 'POST'])  #用户主页面
+def index():
+    return render_template('index.html')
+
+
+
+
+
+
+
+
+
+@app.route('/custom-validator', methods=['GET', 'POST'])
+def custom_validator():
+    form = FortyTwoForm()
+    if form.validate_on_submit():
+        flash('Bingo!')
+        return redirect(url_for('index'))
+    return render_template('custom_validator.html', form=form)
+
+
+@app.route('/uploads/<path:filename>')
+def get_file(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+
+@app.route('/uploaded-images')
+def show_images():
+    return render_template('uploaded.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+def random_filename(filename):
+    ext = os.path.splitext(filename)[1]
+    new_filename = uuid.uuid4().hex + ext
+    return new_filename
+
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    form = UploadForm()
+    if form.validate_on_submit():
+        f = form.photo.data
+        filename = random_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        flash('Upload success.')
+        session['filenames'] = [filename]
+        return redirect(url_for('show_images'))
+    return render_template('upload.html', form=form)
+
+
+
+
+@app.route('/dropzone-upload', methods=['GET', 'POST'])
+def dropzone_upload():
     if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return 'This field is required.', 400
         f = request.files.get('file')
-        file_path = os.path.join(app.config['UPLOADED_PATH'], f.filename)
-        f.save(file_path)
-        # You can return a JSON response then get it on client side:
-        # (see template index.html for client implementation)
-        # return jsonify(uploaded_path=file_path)
-    return render_template('upload.html')
+
+        if f and allowed_file(f.filename):
+            filename = random_filename(f.filename)
+            f.save(os.path.join(
+                app.config['UPLOAD_PATH'], filename
+            ))
+        else:
+            return 'Invalid file type.', 400
+    return render_template('dropzone.html')
 
 
-if __name__ == '__main__':
-    app = Flask(__name__)
-    app.config.update(
-        UPLOADED_PATH=os.path.join(file_dir, 'uploads'),
-        # Flask-Dropzone config:
-        DROPZONE_ALLOWED_FILE_CUSTOM=True,
-        # app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*, .pdf, .txt'
-        DROPZONE_ALLOWED_FILE_TYPE='.pdf,.txt',
-        DROPZONE_MAX_FILE_SIZE=5,
-        DROPZONE_MAX_FILES=30,
-    )
-    dropzone = Dropzone()
-    dropzone.init_app(app)
-    app.register_blueprint(bp)
-    init_db()
-    app.run()
+@app.route('/resources', methods=['GET', 'POST'])
+def resources():
+    dirpath = app.config['UPLOAD_PATH']
+    dirs = os.listdir(dirpath)
+    files = []
+    for file in dirs:
+        files.append(dirpath+"\\"+file)
+    return render_template('resources.html',files =files)
+
+
+
+
+
+
+
+
+@app.route('/ckeditor', methods=['GET', 'POST'])
+def integrate_ckeditor():
+    form = RichTextForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        flash('Your post is published!')
+        return render_template('post.html', title=title, body=body)
+    return render_template('ckeditor.html', form=form)
+
+
+# handle image upload for ckeditor
+@app.route('/upload-ck', methods=['POST'])
+def upload_for_ckeditor():
+    f = request.files.get('upload')
+    if not allowed_file(f.filename):
+        return upload_fail('Image only!')
+    f.save(os.path.join(app.config['UPLOAD_PATH'], f.filename))
+    url = url_for('get_file', filename=f.filename)
+    return upload_success(url, f.filename)
