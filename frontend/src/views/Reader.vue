fc973b5aa129559e7840a272f98cb5f0752308ce
雷超晶 <10184500631@stu.ecnu.edu.cn>
2021-07-22 14:33:00 +0800

    add pdf.js,but cannot run

---
+++ frontend/src/views/Reader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="reader">
+    <MyReader  msg="Welcome to Your Reader"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MyReader from '@/components/MyReader.vue'
+import mdContract from '@/components/contract.md.vue'
+
+export default {
+  name: 'Reader',
+  components: {
+    MyReader,
+    mdContract
+  },
+  data: {
+    abc: ""
+  },
+  methods: {
+    fun1(){
+      this.abc 
+    }
+  }
+}
+</script>
