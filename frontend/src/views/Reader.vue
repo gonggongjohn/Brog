91a09075ea9fc21a9f7ee577fec7f05ce93877af
unknown <758725271@qq.com>
2021-07-29 12:05:31 +0800

    update: demo img, but layout unmodified

--- frontend/src/views/Reader.vue
+++ frontend/src/views/Reader.vue
@@ -2,17 +2,23 @@
   <div class="row">
     <div ref="main_reader">
       <b-card no-body class="col-6">
-              <canvas v-for="page in pageNum" :id="'canvas' + page" :key="page"></canvas>
+        <!-- <canvas
+          v-for="page in pageNum"
+          :id="'canvas' + page"
+          :key="page"
+        ></canvas> -->
+        <img :src=zhouzhihua>
       </b-card>
     </div>
     <div ref="ref_reader">
-      
+      <img :src=huangdingjiang1>
+      <img :src=huangdingjiang2>
     </div>
   </div>
 </template>
 
 <script>
-import * as PDFJS from 'pdfjs-dist/webpack'
+import * as PDFJS from "pdfjs-dist/webpack";
 
 export default {
   name: "Reader",
@@ -22,20 +28,27 @@ export default {
       pdf_url: "",
       pdf_doc: null,
       pageNum: 0,
+      huangdingjiang1: "",
+      huangdingjiang2: "",
+      zhouzhihua: "",
     };
   },
   mounted() {
     this.uuid = this.$route.query.uuid;
-    this.pdf_url = this.getHostUrl() + ':5000/file/get_pdf?book_id=' + this.uuid;
+    this.pdf_url =
+      this.getHostUrl() + ":5000/file/get_pdf?book_id=" + this.uuid;
     this.loadPdf(this.pdf_url);
+    this.zhouzhihua = this.getHostUrl() + ":5000/test/demo_img?pic_name=zhouzhihua";
+    this.huangdingjiang1 = this.getHostUrl() + ":5000/test/demo_img?pic_name=huangdingjiang1";
+    this.huangdingjiang2 = this.getHostUrl() + ":5000/test/demo_img?pic_name=huangdingjiang2";
   },
   methods: {
-    getHostUrl(){
+    getHostUrl() {
       let full_path = window.document.location.href;
       let protocol_index = full_path.indexOf("://");
       let protocol_str = full_path.substring(0, protocol_index);
       let full_path_stripped = full_path.substring(protocol_index + 3);
-      let router_path =  this.$route.path;
+      let router_path = this.$route.path;
       let host_index = full_path_stripped.indexOf(router_path);
       let full_host = full_path_stripped.substring(0, host_index);
       console.log(full_path);
@@ -43,7 +56,7 @@ export default {
       let pure_host = full_host.substring(0, pred_index);
       return protocol_str + "://" + pure_host;
     },
-    loadPdf(pdf_url){
+    loadPdf(pdf_url) {
       let that = this;
       PDFJS.getDocument(pdf_url).promise.then((pdf) => {
         that.pdf_doc = pdf;
@@ -53,27 +66,27 @@ export default {
         });
       });
     },
-    renderPage(page_num){
+    renderPage(page_num) {
       let that = this;
       this.pdf_doc.getPage(page_num).then((page) => {
-        var scale = 1.5
-        var viewport = page.getViewport({scale: scale});
-        var canvas = document.getElementById('canvas' + page_num);
-        var context = canvas.getContext('2d');
+        var scale = 1.5;
+        var viewport = page.getViewport({ scale: scale });
+        var canvas = document.getElementById("canvas" + page_num);
+        var context = canvas.getContext("2d");
         canvas.height = viewport.height;
         canvas.width = viewport.width;
         var renderContext = {
           canvasContext: context,
           viewport: viewport,
-        }
+        };
         page.render(renderContext);
-        console.log(page_num)
+        console.log(page_num);
 
         if (that.pageNum > page_num) {
-          that.renderPage(page_num + 1)
+          that.renderPage(page_num + 1);
         }
       });
-    }
-  }
-}
+    },
+  },
+};
 </script>
